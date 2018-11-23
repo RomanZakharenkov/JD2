82c016ee5c4d2f59b46256e615d388487d62050e
Roma <romeo.zah.93@gmail.com>
2018-11-23 15:52:21 +0300

    deleted DAO

--- database/src/main/java/com/zakharenkov/shop/database/dao/CategoryDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.zakharenkov.shop.database.dao;
-
-import com.zakharenkov.shop.database.model.Category;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CategoryDaoImpl extends BaseDaoImpl<Integer, Category> implements CategoryDao {
-
-}
